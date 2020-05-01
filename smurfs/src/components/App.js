232bd41465cd20c0469105e77b4a5dbbf4b743c1
Andrew Haberman <a.haberman25@gmail.com>
2020-05-01 19:55:38 -0400

    MVP and delete smurf function added, questions

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,15 +1,38 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+// Context
+import { SmurfContext } from "../contexts/SmurfContext";
+
+import Smurf from "./Smurf";
 
 import "./App.css";
 
 function App() {
+  const [smurfs, setSmurfs] = useState();
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then((res) => {
+        console.log("axios res ", res.data);
+        setSmurfs(res.data);
+      })
+      .catch((err) => {
+        console.log("error: ", err);
+      });
+  }, []);
+
+  console.log("smurfs ", smurfs);
 
   return (
     <div className="App">
       <h1>SMURFS! 2.0 W/ Redux</h1>
+      <SmurfContext.Provider value={{ smurfs }}>
+        <Smurf />
+      </SmurfContext.Provider>
     </div>
   );
 }
 
 export default App;
-
